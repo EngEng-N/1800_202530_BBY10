8d0da76a0f8c82adde7904de828ce22591fefddb
EngEng-N <eeng3906@gmail.com>
2025-11-19 18:01:00 -0800

    removed top nav in pages that don't need it

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -3,7 +3,9 @@ export function createTopNav() {
   topNav.classList.add("navbar");
 
   topNav.innerHTML = `
-        <a href="" class="logo">LifeSync</a>
+        <a href="#" class="logo navbar-brand">
+          <img src="images/logo.png" alt="LifeSync Logo" height="40" />
+        </a>
         <div class="hamburger" id="hamburger">
             <span></span>
             <span></span>
